069fddb5be6c2e6c041626f4e29b0043fb53fd35
jerry zhang <171250022@smail.nju.edu.cn>
2019-06-01 19:23:18 +0800

    'l'

--- theThird/src/main/resources/static/js/adminCoupons.js
+++ theThird/src/main/resources/static/js/adminCoupons.js
@@ -1,27 +1,28 @@
 $(document).ready(function() {
 
     var canNum = 100;
+    var buyNum=100;
     var members=[];
 
     getCouponsNum();
-    getMemberList('');
+    getBuyNum();
     //以下为更改优惠金额的方法
     function getCouponsNum() {
 
         //该行需注释掉
-        $("#send-num").text(canNum);
+        // $("#send-num").text(canNum);
 
         //以下为与后端交互真方法
-        // getRequest(
-        //     '/coupon/getNum',
-        //     function (res) {
-        //         canNum = res.content;
-        //         $("#send-num").text(canNum);
-        //     },
-        //     function (error) {
-        //         alert(JSON.stringify(error));
-        //     }
-        // );
+        getRequest(
+            '/coupon/getNum',
+            function (res) {
+                canNum = res.content;
+                $("#send-num").text(canNum);
+            },
+            function (error) {
+                alert(JSON.stringify(error));
+            }
+        );
     }
 
     $('#send-modify-btn').click(function () {
@@ -36,58 +37,86 @@ $(document).ready(function() {
         var dayNum = $("#send-set-input").val();
 
         // 以下需注释掉
-        canNum = dayNum;
-        getCouponsNum();
-        $("#send-modify-btn").show();
-        $("#send-set-input").hide();
-        $("#send-confirm-btn").hide();
+        // canNum = dayNum;
+        // getCouponsNum();
+        // $("#send-modify-btn").show();
+        // $("#send-set-input").hide();
+        // $("#send-confirm-btn").hide();
 
         //以下为与后端交互真方法
-        // postRequest(
-        //     '/coupon/change',
-        //     {day:dayNum},
-        //     function (res) {
-        //         if(res.success){
-        //             canNum = dayNum;
-        //             getCouponsNum();
-        //             $("#send-modify-btn").show();
-        //             $("#send-set-input").hide();
-        //             $("#send-confirm-btn").hide();
-        //         } else{
-        //             alert(res.message);
-        //         }
-        //     },
-        //     function (error) {
-        //         alert(JSON.stringify(error));
-        //     }
-        // );
+        postRequest(
+            '/coupon/change',
+            {day:dayNum},
+            function (res) {
+                if(res.success){
+                    canNum = dayNum;
+                    getCouponsNum();
+                    $("#send-modify-btn").show();
+                    $("#send-set-input").hide();
+                    $("#send-confirm-btn").hide();
+                } else{
+                    alert(res.message);
+                }
+            },
+            function (error) {
+                alert(JSON.stringify(error));
+            }
+        );
+    })
+    //以下为修改最低消费的方法
+    function getBuyNum() {
+        $("#buy-num").text(buyNum);
+    }
+
+    $('#buy-modify-btn').click(function () {
+        $("#buy-modify-btn").hide();
+        $("#buy-set-input").val(canNum);
+        $("#buy-set-input").show();
+        $("#buy-confirm-btn").show();
+    });
+    //z
+
+    $('#buy-confirm-btn').click(function () {
+        var bNum = $("#buy-set-input").val();
+
+        // 以下需注释掉
+        // var list=[{
+        //     username:"ezio",
+        //     password:123456,
+        //     id:1
+        // },{
+        //     username:"router",
+        //     password:123456,
+        //     id:2
+        // }]
+        // renderMemberList(list);
+        // buyNum = bNum;
+        // getBuyNum();
+        // $("#buy-modify-btn").show();
+        // $("#buy-set-input").hide();
+        // $("#buy-confirm-btn").hide();
+
+        //以下为与后端交互真方法
+        postRequest(
+            '/coupon/changeBuyNum',
+            {buyNum:buyNum},
+            function (res) {
+                var list=res.content;
+                renderMemberList(list);
+                buyNum = bNum;
+                getBuyNum();
+                $("#buy-modify-btn").show();
+                $("#buy-set-input").hide();
+                $("#buy-confirm-btn").hide();
+
+            },
+            function (error) {
+                alert(JSON.stringify(error));
+            }
+        );
     })
 
-
     //以下为展示用户的方法
-    function getMemberList() {
-        //以下需注释掉
-        var list=[{
-            username:"ezio",
-            password:123456,
-            id:1
-        },{
-            username:"router",
-            password:123456,
-            id:2
-        }]
-        renderMemberList(list);
-
-        //以下为与后端交互真方法
-        // getRequest(
-        //     '/coupon/allMember',
-        //     function (res) {
-        //         renderMemberList(res.content);
-        //     },
-        //     function (error) {
-        //         alert(error);
-        //     });
-    }
 
     function renderMemberList(list) {
         $('.member-card').empty();
@@ -110,20 +139,26 @@ $(document).ready(function() {
         $("#myTable").append(memberDomStr);
     }
 //以下为选中某些用户的方法
-    $('#allOptionId').on('ifChecked', function(event){
-        alert("全选");
-        $('input').iCheck('check');
-    });
-    $('#allOptionId').on('ifUnchecked', function(event){
-        alert("反选");
-        $('input').iCheck('uncheck');
-    });
-    $('input[name="optionName"]:checked').each(function() {
-        alert("单选");
-        members.push($(this).val());
-        console.log(members);
-    });
+//     $('#allOptionId').on('ifChecked', function(event){
+//         alert("全选");
+//         $('input').iCheck('check');
+//     });
+//     $('#allOptionId').on('ifUnchecked', function(event){
+//         alert("反选");
+//         $('input').iCheck('uncheck');
+//     });
+//     $('input').on('ifChecked', function(event){
+//         alert(event.type + ' callback');
+//     });
+//     $('input[name="optionName"]:checked').each(function() {
+//         alert("单选");
+//         members.push($(this).val());
+//         console.log(members);
+//     });
+    if($("input").prop("checked") == true){
+        alert("danxuan");
+    }
     sendConfirmClick=function(){
-
+        alert("还没实现");
     }
 });
